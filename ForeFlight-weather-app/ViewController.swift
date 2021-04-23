e56eb7bbd3ce5ebd144763a9a2c5e555d1b265f5
Alex Beaty <romeobeaty@gmail.com>
2021-04-23 10:31:21 -0500

    Initial Commit

---
+++ ForeFlight-weather-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForeFlight-weather-app
+//
+//  Created by Alexander Scott Beaty on 4/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
